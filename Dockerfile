8d86b996c310b37affee8dc6f6e729cd61ca7de9
Alexandre Gauthier <alex@lab.underwares.org>
2018-09-30 09:15:52 -0400

    Initial import

---
+++ Dockerfile
@@ -0,0 +1,40 @@
+FROM debian:9-slim
+
+LABEL maintainer="Alexandre Gauthier <alex@lab.underwares.org>" \
+    description="Factorio Server"
+
+ARG VERSION="0.16.51"
+ARG SHA256="6cb09f5ac87f16f8d5b43cef26c0ae26cc46a57a0382e253dfda032dc5bb367f"
+ARG URL="https://www.factorio.com/get-download/${VERSION}/headless/linux64"
+
+ENV FACTORIO_HOME /opt/factorio
+ENV FACTORIO_PORT 34197
+ENV FACTORIO_RCON_PORT 27015
+ENV FACTORIO_SAVESDIR ${FACTORIO_HOME}/saves
+ENV FACTORIO_CONFIGDIR ${FACTORIO_HOME}/config
+ENV FACTORIO_MODSDIR ${FACTORIO_HOME}/mods
+
+# Create runtime directories
+RUN mkdir -p /opt/factorio/{saves,config,mods}
+
+WORKDIR /opt/factorio
+
+# Install runtime dependencies
+RUN apt-get update && apt-get install --no-install-recommends -y \
+    curl ca-certificates xz-utils pwgen && \
+    apt-get clean && \
+    rm -rf /var/lib/apt/lists/* && rm -rf /tmp/* && rm -rf /var/tmp/*
+
+# Download, verify and extract headless server archive 
+RUN curl -L $URL -o /tmp/archive_${VERSION}.txz && \
+    echo "${SHA256}  /tmp/archive_${VERSION}.txz" | sha256sum -c - && \
+    tar xvJf /tmp/archive_${VERSION}.txz -C /opt && \
+    rm -f /tmp/archive_${VERSION}.txz
+
+COPY scripts/run.sh .
+
+VOLUME [${FACTORIO_SAVESDIR}, ${FACTORIO_MODSDIR}, ${FACTORIO_CONFIGDIR}]
+
+EXPOSE ${FACTORIO_PORT}/udp ${FACTORIO_RCON_PORT}/tcp
+
+ENTRYPOINT [ "/opt/factorio/run.sh" ]
---
+++ scripts/run.sh
@@ -0,0 +1,83 @@
+#!/usr/bin/env bash
+# Factorio Launcher
+# A wrapper script so basic it might as well wear a northface jacket
+
+PWGEN="/usr/bin/pwgen"
+
+## Sanity Checks
+
+# Don't run as root within the container, also indicates lack of config
+if [[ $UID -eq 0 ]] ; then
+    >&2 echo "ERROR: Refusing to start as root (uid 0)."
+    >&2 echo "  Verify your runtime configuration."
+    exit 1
+fi
+
+for d in "$FACTORIO_SAVESDIR" "$FACTORIO_CONFIGDIR" "$FACTORIO_MODSDIR" ; do
+    if [[ ! -w $d ]] ; then
+        >&2 echo "ERROR: Directory \"$d\" is not writable"
+        >&2 echo "  Did you set permissions on the volume correctly?"
+        >&2 echo "  Is the container configured to run as the correct user?"
+        exit 1
+    fi
+done
+
+## Initial run generation
+
+# RCON password
+if [[ ! -f $FACTORIO_CONFIGDIR/rconpw ]] ; then
+    echo "Generating initial rcon password..."
+    $PWGEN 15 1 > $FACTORIO_CONFIGDIR/rconpw || exit 1
+fi
+
+# Configuration files
+mkdir -p $FACTORIO_CONFIGDIR/example
+
+for i in server-settings map-gen-settings map-settings ; do
+    echo "Refreshing example ${i} with latest from distribution"
+    cp $FACTORIO_HOME/data/${i}.example.json $FACTORIO_CONFIGDIR/
+
+    # In case of missing config, create from defaults
+    if [[ ! -f $FACTORIO_CONFIGDIR/${i}.json ]] ; then
+        >&2 echo "WARNING: ${i}.json not found, creating from example"
+        cp -v $FACTORIO_HOME/data/${i}.example.json \
+            $FACTORIO_CONFIGDIR/${i}.json || exit 1
+    fi
+fi
+
+# Save Files
+find -L $FACTORIO_SAVESDIR -type f -iname '*.tmp.zip' -exec \
+    rm -f {} \;
+
+if [[ ! -f ${FACTORIO_SAVESDIR}/save.zip ]] ; then
+    $FACTORIO_HOME/bin/x64/factorio \
+        --create $FACTORIO_SAVESDIR/save.zip \
+        --map-gen-settings $FACTORIO_CONFIGDIR/map-gen-settings.json \
+        --map-settings $FACTORIO_CONFIGDIR/map-settings.json
+    echo "Initial map created using $FACTORIO_CONFIGDIR/map*.json"
+fi
+
+echo "-----------------------------------------------------------------------"
+echo "$($FACTORIO_HOME/bin/x64/factorio --version | grep 'Version:')"
+echo -ne "\n"
+echo "FACTORIO_HOME: $FACTORIO_HOME"
+echo "FACTORIO_PORT: $FACTORIO_PORT"
+echo "FACTORIO_RCON_PORT: $FACTORIO_RCON_PORT"
+echo "FACTORIO_SAVESDIR: $FACTORIO_RCON_PORT"
+echo "FACTORIO_CONFIGDIR: $FACTORIO_CONFIGDIR"
+echo "FACTORIO_MODSDIR: $FACTORIO_MODSDIR"
+echo -ne "\n"
+echo "-----------------------------------------------------------------------"
+
+# Hand off to server binary
+exec $FACTORIO_HOME/bin/x64/factorio \
+    --port $FACTORIO_PORT \
+    --rcon-port $FACTORIO_RCON_PORT \
+    --rcon-password "$(cat $FACTORIO_CONFIGDIR/rconpw)" \
+    --server-settings "$FACTORIO_CONFIGDIR/server-settings.json" \
+    --server-whitelist "$FACTORIO_CONFIGDIR/server-whitelist.json" \
+    --server-banlist "$FACTORIO_CONFIGDIR/server-banlist.json" \
+    --start-server-load-latest \
+    --server-id "$FACTORIO_CONFIGDIR/server-id.json" \
+    $@
+
